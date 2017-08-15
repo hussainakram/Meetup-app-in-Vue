fdb77030879424e5275002d76dfa3ae65298ae5c
Hussain Akram <hussain.akram128@gmail.com>
2017-08-15 17:19:21 +0500

    makes signup functional with firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as firebase from 'firebase'
 
 Vue.use(Vuex)
 
@@ -31,14 +32,14 @@ export const store = new Vuex.Store({
         description: "It'll be an awesome Meetup!!"
       }
     ],
-    user: {
-      id: '121',
-      registeredMeetups: ['aqaqa']
-    }
+    user: null
   },
   mutations: {
     createMeetup (state, payload) {
       state.loadedMeetups.push(payload)
+    },
+    setUser (state, payload) {
+      state.user = payload
     }
   },
   actions: {
@@ -52,6 +53,21 @@ export const store = new Vuex.Store({
         id: payload.id
       }
       commit('createMeetup', meetup)
+    },
+    signUserUp ({ commit }, payload) {
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+      .then(
+        user => {
+          const newUser = {
+            id: user.uid,
+            registeredMeetups: []
+          }
+          commit('setUser', newUser)
+        }
+      )
+      .catch(error => {
+        console.log(error)
+      })
     }
   },
   getters: {
@@ -69,6 +85,9 @@ export const store = new Vuex.Store({
           return meetup.id === meetupId
         })
       }
+    },
+    user (state) {
+      return state.user
     }
   }
 })
